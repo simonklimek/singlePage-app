6e5a9ee8530413d24947e1d8f8057fa146075c2b
simonklimek <simonklimek91@gmail.com>
2019-03-12 21:57:59 +0000

    amended front end with methods

--- app.js
+++ app.js
@@ -1,33 +1,18 @@
-const express = require('express')
-const morgan = require('morgan')
+const express = require('express');
+// const morgan = require('morgan')
 const mysql = require('mysql');
 
 // setting up the database 
 const db = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "",
-  database: "nodejs_db"
+  password: "root",
+  database: "mysql_alfa",
+  port: 8889
 });
 
 const app = express();
-app.use(morgan('tiny'));
-
-// db.connect((err)=> {
-//     if (err){
-//         throw err;
-//     }
-//     console.log("Connected to MYSQL.....!");
-//     db.query("SELECT * FROM users", (err, result, fields)=> {
-//       if (err){
-//           throw err;
-//         }
-//     // console.log(JSON.stringify(result));
-//     console.log(result);
-//     });
-//   });
-
-
+// app.use(morgan('tiny'));
 
 // db.connect();
 
@@ -40,32 +25,67 @@ app.use(morgan('tiny'));
 
 // db.end();
 
-db.connect(function(err){
+db.connect((err) => {
     if(!err) {
-        console.log("Database is connected ... nn");    
+        console.log("Database is connected ... !!");    
     } else {
-        console.log("Error connecting database ... nn");    
+        console.log("Error connecting database ... !!");    
     }
     });
 
+
+// Function that add data to database
+function addData() {
+  // SQL query
+  var sql = "INSERT INTO users (name, email, password) " + " VALUES ('Mark', 'mark@interia.pl', 'pass3')";
+
+  db.query(sql, function (err, result) {
+    // Check for errors
+    if (err) throw err;
+
+    // Output results
+    console.log(result.affectedRows + 'wors updated. ID is ' + result.insertId);
+  });
+};
+
+// addData();
+
+// Routes
+
 app.get("/",function(req,res){
     db.query('SELECT * FROM users LIMIT 2', function(err, result, fields) {
-    db.end();
+
       if (!err)
         console.log('The solution is: ', result);
-      else
+      else {
         console.log('Error while performing Query.');
-      });
+      }});
     });
     
+app.get("/u",(req,res) => {
+  db.query("SELECT name FROM users", (err, result)=> {
+    if (err){
+        console.log(`custom error type: ${err}`);
+      } else {
+  console.log(JSON.stringify(result));
+  console.log('------------------------------');
+  // console.log(result);
+  res.json([result]);
+  db.end();
+  }});
+  });
+
+// json example
+app.get("/users", (req,res)=>{
+  const user1 = {name: "Simon", occupation: "WebDev", age: 27}
+  const user2 = {name: "Claire", occupation: "Designer", age: 22}
+  res.json([user1,user2])
+});
+
+
+
+
 app.listen(3000);
-
-// routes
-// app.get("/", (req,res)=>{
-//     console.log("Responding to root route.....")
-//     res.send("HI FROM ROOT DIR")
-// })
-
 
 // app.get("/createdb", (req,res)=>{
 //     let sql = "CREATE DATABASE nodemysql";
@@ -89,13 +109,4 @@ app.listen(3000);
 
 
 
-// // json example
-// app.get("/users", (req,res)=>{
-//     const user1 = {name: "Simon", occupation: "WebDev", age: 27}
-//     const user2 = {name: "Claire", occupation: "Designer", age: 22}
-//     res.json([user1,user2])
-// })
 
-// app.listen(3003, ()=>{
-//     console.log("Server is up and listening on 3003.....");
-// });
