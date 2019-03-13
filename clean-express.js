f7416072f2aa56a322da345a182be4a785a2a5f0
simonklimek <simonklimek91@gmail.com>
2019-03-13 20:07:10 +0000

    pug implementation views

---
+++ clean-express.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.get('/', function(req, res) {
+    let articles = [
+        {
+            id:1,
+            title:'Article title',
+            author:'Simon Klimek',
+            body:'This is the blog article one'
+        },
+        {
+            id:2,
+            title:'Article title two',
+            author:'Arni Klimek',
+            body:'This is the blog article two'
+        },
+        {
+            id:3,
+            title:'Article title three',
+            author:'Tobias Klimek',
+            body:'This is the blog article three'
+        }
+    ];
+    
+    
+    res.render('index', {
+        title: "Articles list",
+        articles: articles
+    });
+});
+
+app.get('/articles/add', function(req, res) {
+    res.render('add_article', {
+        title: "Add Articles"
+    });
+})
+
+app.listen(3000,function() {
+    console.log('server started');
+})
