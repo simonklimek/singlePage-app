78474ceb6d1069b111f42a5920f382821a3932c1
simonklimek <simonklimek91@gmail.com>
2019-03-18 17:47:37 +0000

    post button feature fix js

--- public/js/post-btn.js
+++ public/js/post-btn.js
@@ -3,6 +3,21 @@
 //     var instances = M.FloatingActionButton.init(elems);
 //   });
 
+// document.addEventListener('DOMContentLoaded', function () {
+//   var elems = document.querySelectorAll('.modal');
+//   var instances = M
+//       .Modal
+//       .init(elems);
+// });
+
+// Or with jQuery
+
+// $(document).ready(function () {
+//   $('.modal').modal();
+// });
+
+
+
 document.addEventListener('DOMContentLoaded', function () {
   var elems = document.querySelectorAll('.modal');
   var instances = M
